b63412986f7488647c8aea28a5b8b2cd8c48e3ff
Peter Jahelka <prjahelka@msn.com>
2017-04-30 09:52:49 -0600

    somewhat working

---
+++ server/server.js
@@ -0,0 +1,80 @@
+// var express = require('express');
+// var bodyParser = require('body-parser');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
+
+// var app = express();
+
+// app.use(bodyParser.json());
+
+// app.post('/todos', (req, res) => {
+//     console.log('req.body: ', req.body);
+//     var todo = new ToDo();
+//     todo.text = req.body.text;
+//     todo.save().then((doc) => {
+//         res.send(doc);
+//     }, (err) => {
+//         res.status(400).send(err);
+//     });
+// });
+
+// app.listen(3000, () => {
+//     console.log('Started on port 3000');
+// });
+
+// // var user = new User({});
+
+// // user.save().then((doc) => {
+// //     console.log('Saved: ', doc);
+// // }, (err) => {
+// //     console.log(err);
+// // });
+
+// // var user = new User({
+// //     email: '  prjahelka@msn.com   '
+// // });
+
+// // user.save().then((doc) => {
+// //     console.log('Saved: ', doc);
+// // }, (err) => {
+// //     console.log(err);
+// // });
+
+// // var todo = new Todo({
+// //     title: 'dinner',
+// //     text: 'cook dinner',
+// //     completed: false,
+// //     completedAt: 1530
+// // });
+
+// // todo.save().then((doc) => {
+// //     console.log('Saved: ', doc);
+// // }, (err) => {
+// //     console.log(err);
+// // });
