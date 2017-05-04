9f5fd9741d5a19b904393bca4730ffe0fe93205e
Peter Jahelka <prjahelka@msn.com>
2017-05-04 06:50:34 -0600

    setup for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var { ObjectID } = require('mongodb');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -50,58 +52,8 @@ app.get('/todos/:id', (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = { app };
-
-// var app = express();
-
-// app.use(bodyParser.json());
-
-// app.post('/todos', (req, res) => {
-//     console.log('req.body: ', req.body);
-//     var todo = new ToDo();
-//     todo.text = req.body.text;
-//     todo.save().then((doc) => {
-//         res.send(doc);
-//     }, (err) => {
-//         res.status(400).send(err);
-//     });
-// });
-
-// app.listen(3000, () => {
-//     console.log('Started on port 3000');
-// });
-
-// // var user = new User({});
-
-// // user.save().then((doc) => {
-// //     console.log('Saved: ', doc);
-// // }, (err) => {
-// //     console.log(err);
-// // });
-
-// // var user = new User({
-// //     email: '  prjahelka@msn.com   '
-// // });
-
-// // user.save().then((doc) => {
-// //     console.log('Saved: ', doc);
-// // }, (err) => {
-// //     console.log(err);
-// // });
-
-// // var todo = new Todo({
-// //     title: 'dinner',
-// //     text: 'cook dinner',
-// //     completed: false,
-// //     completedAt: 1530
-// // });
-
-// // todo.save().then((doc) => {
-// //     console.log('Saved: ', doc);
-// // }, (err) => {
-// //     console.log(err);
-// // });
