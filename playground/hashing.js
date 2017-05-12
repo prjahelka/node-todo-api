e8e8860ca53909eb76c0d3026ed64cb741518846
Peter Jahelka <prjahelka@msn.com>
2017-05-12 13:35:18 -0600

    bcrypt hasing user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,27 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 4
-};
-var token = jwt.sign(data, '123abc');
-console.log('token: ', token);
-var decoded = jwt.decode(token, '123abc');
-console.log('decoded: ', decoded);
+var password = 'abc123!';
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log('the hash:', hash);
+//     })
+// });
+
+var theHash = '$2a$10$C3kG84MY8ieRe3EICGDSfe5akJeVLuM5h5wwvK.W57r1mxN.6zoGK';
+
+bcrypt.compare(password, theHash, (err, result) => {
+    console.log('compare result: ', result);
+})
+
+// var data = {
+//     id: 4
+// };
+// var token = jwt.sign(data, '123abc');
+// console.log('token: ', token);
+// var decoded = jwt.decode(token, '123abc');
+// console.log('decoded: ', decoded);
 
 // var message = "I am a typical user";
 // var hash = SHA256(message).toString();
